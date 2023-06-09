8a2507e90d8298e5ea6d576546e5a8e9e6b2657a
coo1m <254503963@qq.com>
2023-06-10 00:14:38 +0800

    修复某些机型状态栏未透明的问题，如oppo、一加，之前的写法是错误的

--- app/src/main/java/com/master/sr/RootActivity.kt
+++ app/src/main/java/com/master/sr/RootActivity.kt
@@ -1,37 +1,52 @@
 package com.master.sr
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Surface
+import androidx.core.view.WindowCompat
 import com.hjq.permissions.OnPermissionCallback
 import com.hjq.permissions.Permission
 import com.hjq.permissions.XXPermissions
-import com.master.sr.ui.AppTheme
-import com.master.sr.utils.TwUtil
+import com.master.sr.theme.AppTheme
+import com.master.sr.util.TwUtil
 import com.master.sr.view.MainScreen
 
 class RootActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        XXPermissions.with(this)
-            .permission(Permission.READ_MEDIA_IMAGES)
-            .request(object : OnPermissionCallback {
-                override fun onGranted(permissions: MutableList<String>, all: Boolean) {
-                    if (all) {
-                        setContent { AppTheme { Surface { MainScreen() } } }
-                    }
-                }
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.attributes.layoutInDisplayCutoutMode =
+            WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
 
-                override fun onDenied(permissions: MutableList<String>, never: Boolean) {
-                    if (never) {
-                        TwUtil.res(R.string.permit_yourself)
-                        XXPermissions.startPermissionActivity(this@RootActivity, permissions)
-                    } else {
-                        TwUtil.res(R.string.permit_refuse)
+        if (XXPermissions.isGranted(this, Permission.READ_MEDIA_IMAGES)) {
+            content()
+        } else {
+            XXPermissions.with(this)
+                .permission(Permission.READ_MEDIA_IMAGES)
+                .request(object : OnPermissionCallback {
+                    override fun onGranted(permissions: MutableList<String>, all: Boolean) {
+                        if (all) content()
                     }
-                }
-            })
+
+                    override fun onDenied(permissions: MutableList<String>, never: Boolean) {
+                        if (never) {
+                            TwUtil.short(R.string.permit_yourself)
+                            XXPermissions.startPermissionActivity(this@RootActivity, permissions)
+                        } else {
+                            TwUtil.short(R.string.permit_refuse)
+                        }
+                    }
+                })
+        }
     }
+
+    fun content() {
+        setContent { AppTheme { MainScreen() } }
+    }
+
 }
