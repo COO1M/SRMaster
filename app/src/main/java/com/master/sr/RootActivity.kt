26d2caff3e34eeee7b7385abbbe5ab24dac09f3d
coo1m <254503963@qq.com>
2023-06-10 21:51:02 +0800

    适配SplashScreen

--- app/src/main/java/com/master/sr/RootActivity.kt
+++ app/src/main/java/com/master/sr/RootActivity.kt
@@ -3,6 +3,7 @@ package com.master.sr
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import com.hjq.permissions.OnPermissionCallback
 import com.hjq.permissions.Permission
@@ -15,6 +16,7 @@ class RootActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        installSplashScreen()
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         if (XXPermissions.isGranted(this, Permission.READ_MEDIA_IMAGES)) {
